c519b05ddf45f0196660da9c955467b24d98deb5
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-01-21 13:41:25 +0100

    Finishing Cart

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography, Icon } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+
+import useStyles from './styles'
+
+import logo from '../../assets/pyramid-shape.png'
+
+
+const Navbar = ({ totalItems }) => {
+
+    const classes = useStyles();
+
+    return (
+        <>
+            <AppBar position="fixed" className={classes.appBar} color="inherit">
+                <Toolbar>
+                    <Typography varient="h6" className={classes.title} color="inherit">
+                        <img src={logo} alt="DevineShop" height="25xp" className={classes.image} />
+                        DevineShop
+                    </Typography>
+                    <div className={classes.grow} />
+                        <div className={classes.button}>
+                        <IconButton>
+                            <Badge badgeContent={totalItems} color="secondary">
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                        </div>
+                </Toolbar>
+            </AppBar>
+            
+        </>
+    )
+}
+
+export default Navbar;

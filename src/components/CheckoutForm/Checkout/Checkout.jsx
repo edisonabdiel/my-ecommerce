bbeda2e2791ed0c7cab2bc5fe4c5b2f8f8a6c52e
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-02-04 14:25:32 +0100

    PaymentForm + Review items + Stripe intergration

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -10,7 +10,7 @@ import useStyles from './styles'
 
 const steps = ['Shipping adress', 'Payment details'];
 
-const Checkout = ({ cart }) => {
+const Checkout = ({ cart, order, onCaptureCheckout, error }) => {
     const [activeStep, setActiveStep] = useState(0);
     const [checkoutToken, setCheckoutToken] = useState(null);
     const [shippingData, setShippingData] = useState({});
@@ -49,7 +49,7 @@ const Checkout = ({ cart }) => {
 
     const Form = () => activeStep === 0
         ? <AdressForm checkoutToken={checkoutToken} next={ next }/>
-        : <PaymentForm shippingData={shippingData} />
+        : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} backStep={backStep} onCaptureCheckout={onCaptureCheckout} nextStep={nextStep} />
 
     return (
         <>
